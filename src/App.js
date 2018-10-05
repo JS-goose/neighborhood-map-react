fc376b1be06ca1086f5b537d8ea0868e6a6947b4
Jonathan Sexton <joncsexton@gmail.com>
2018-10-05 12:26:43 -0500

    added getVenues function with params and axios get function

--- src/App.js
+++ src/App.js
@@ -4,6 +4,25 @@ import Map from './components/Map'
 import axios from 'axios';
 
 class App extends Component {
+
+  getVenues = () => {
+    const endpoint = 'https://api.foursquare.com/v2/venues/explore';
+    const parameters = {
+      clientId: "",
+      clientSecret: "",
+      query: "",
+      section: "",
+      near: "San Antonio"
+    }
+
+    axios.get(endpoint + new URLSearchParams(parameters))
+    .then(response => {
+      console.log(response)
+    }).catch(error => {
+      console.log(`Error in Axios get: ${error}`)
+    });
+
+  }
   render() {
     return (
       <div className="App">
