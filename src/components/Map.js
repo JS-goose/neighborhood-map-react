b9120ccc89e73845b60a30191402dc1f86af86dd
Jonathan Sexton <joncsexton@gmail.com>
2018-10-04 19:08:11 -0500

    created Map component

---
+++ src/components/Map.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Map extends Component {
+    render() {
+        return(
+            <div id="map">
+                <h1>Map Area</h1>
+            </div>
+        )
+    }
+}
+
+export default Map
