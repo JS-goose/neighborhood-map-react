12a95cbefe94e0bef9f83c69d79ddcee8047ba4b
Jonathan Sexton <joncsexton@gmail.com>
2018-10-05 09:02:12 -0500

    Fixed placement of loadScript to be outside of component

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,13 +1,19 @@
 import React, { Component } from "react";
 
-initMap = () => {
-  const map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
-  });
-}
-
 class Map extends Component {
+  // *This function creates a useable script so we can insert Google Maps without using any 3rd party loaders
+  loadMap = () => {
+    loadScript(
+      "https://maps.googleapis.com/maps/api/js?key=AIzaSyCeg7QO_VyQ3FWMQexN8WPLJvGSDXOynuc&callback=initMap"
+    );
+  };
+
+  initMap = () => {
+    let map = new window.google.maps.Map(document.getElementById("map"), {
+      center: { lat: -34.397, lng: 150.644 },
+      zoom: 8,
+    });
+  };
   render() {
     return (
       <main>
@@ -17,15 +23,13 @@ class Map extends Component {
   }
 }
 
-// *This function creates a useable script so we can insert Google Maps without using any 3rd party loaders
 function loadScript(url) {
-    let index = window.document.getElementsByTagName("script")[0];
-    let script = window.document.createElement("script");
-    script.src = url;
-    script.async = true;
-    script.defer = true;
-    index.parentNode.insertBefore(script, index);
-
+  let index = window.document.getElementsByTagName("script")[0];
+  let script = window.document.createElement("script");
+  script.src = url;
+  script.async = true;
+  script.defer = true;
+  index.parentNode.insertBefore(script, index);
 }
 
 export default Map;
