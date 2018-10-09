9ae3e4f748e8caea9569e993039f87e1beae2554
Jonathan Sexton <joncsexton@gmail.com>
2018-10-09 04:54:51 -0500

    Built Nav

---
+++ src/components/Nav.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { slide as Nav } from 'react-burger-menu'
+
+class Nav extends Component {
+    render() {
+        return (
+            <nav>
+                <h1>Neighborhood Maps</h1>
+            </nav>
+        )
+    }
+}
+
+export default Nav
