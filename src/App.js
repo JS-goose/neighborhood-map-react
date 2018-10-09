9a54060ba4d186397720179cef725d6e11a561c6
Jonathan Sexton <joncsexton@gmail.com>
2018-10-09 08:32:47 -0500

    Adjusting nav to be responsive - incomplete

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 import Map from './components/Map'
 import axios from 'axios';
-import Nav from './components/Nav';
+// import Nav from './components/Nav';
 import Sidebar from './components/Sidebar'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Nav />
+        {/* <Nav /> */}
         <Map />
         <Sidebar />
       </div>
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,28 +1,27 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Sidebar extends Component {
-    render() {
-        return (
-            <aside id = "sidebarContainer">
-                <div id = "sidebarList">
-                <ol>
-                    <li>
-                        One
-                    </li>
-                    <li>
-                        Two
-                    </li>
-                    <li>
-                        Three
-                    </li>
-                    <li>
-                        Four
-                    </li>
-                </ol>
-                </div>
-            </aside>
-        )
-    }
+  render() {
+    return (
+      <nav id="navContainer">
+        <div id="nav">
+          <h1 className="header">Neighborhood Maps</h1>
+          <button id="closeNav">X</button>
+        </div>
+
+        <aside id="sidebarContainer">
+          <div id="sidebarList">
+            <ol>
+              <li>One</li>
+              <li>Two</li>
+              <li>Three</li>
+              <li>Four</li>
+            </ol>
+          </div>
+        </aside>
+      </nav>
+    );
+  }
 }
 
-export default Sidebar
+export default Sidebar;
