7ad1512396366a77e9b2f089309a4551cb6a1640
Jonathan Sexton <joncsexton@gmail.com>
2018-10-11 05:46:57 -0500

    Style-Updated the styling of map

--- src/App.js
+++ src/App.js
@@ -32,9 +32,9 @@ class App extends Component {
     const marker = this.state.markers
       .filter((marker) => marker.id === venueListItem.venue.venue.id)
       .map((marker) => {
-        this.state.infowindow.setContent(this.state.content)
+        this.state.infowindow.setContent(this.state.content);
         this.state.infowindow.open(this.initMap, marker);
-        console.log(this.state.content)
+        console.log(this.state.content);
       });
   };
 
@@ -43,6 +43,144 @@ class App extends Component {
     const map = new window.google.maps.Map(document.getElementById("map"), {
       center: { lat: 29.424122, lng: -98.493628 },
       zoom: 11,
+      styles: [
+        {
+          elementType: "geometry",
+          stylers: [
+            {
+              hue: "#ff4400",
+            },
+            {
+              saturation: -68,
+            },
+            {
+              lightness: -4,
+            },
+            {
+              gamma: 0.72,
+            },
+          ],
+        },
+        {
+          featureType: "road",
+          elementType: "labels.icon",
+        },
+        {
+          featureType: "landscape.man_made",
+          elementType: "geometry",
+          stylers: [
+            {
+              hue: "#0077ff",
+            },
+            {
+              gamma: 3.1,
+            },
+          ],
+        },
+        {
+          featureType: "water",
+          stylers: [
+            {
+              hue: "#00ccff",
+            },
+            {
+              gamma: 0.44,
+            },
+            {
+              saturation: -33,
+            },
+          ],
+        },
+        {
+          featureType: "poi.park",
+          stylers: [
+            {
+              hue: "#44ff00",
+            },
+            {
+              saturation: -23,
+            },
+          ],
+        },
+        {
+          featureType: "water",
+          elementType: "labels.text.fill",
+          stylers: [
+            {
+              hue: "#007fff",
+            },
+            {
+              gamma: 0.77,
+            },
+            {
+              saturation: 65,
+            },
+            {
+              lightness: 99,
+            },
+          ],
+        },
+        {
+          featureType: "water",
+          elementType: "labels.text.stroke",
+          stylers: [
+            {
+              gamma: 0.11,
+            },
+            {
+              weight: 5.6,
+            },
+            {
+              saturation: 99,
+            },
+            {
+              hue: "#0091ff",
+            },
+            {
+              lightness: -86,
+            },
+          ],
+        },
+        {
+          featureType: "transit.line",
+          elementType: "geometry",
+          stylers: [
+            {
+              lightness: -48,
+            },
+            {
+              hue: "#ff5e00",
+            },
+            {
+              gamma: 1.2,
+            },
+            {
+              saturation: -23,
+            },
+          ],
+        },
+        {
+          featureType: "transit",
+          elementType: "labels.text.stroke",
+          stylers: [
+            {
+              saturation: -64,
+            },
+            {
+              hue: "#ff9100",
+            },
+            {
+              lightness: 16,
+            },
+            {
+              gamma: 0.47,
+            },
+            {
+              weight: 2.7,
+            },
+          ],
+        },
+      ],
     });
 
     //* Loops over all venue items and dynamically creates markers for them
@@ -81,7 +219,7 @@ class App extends Component {
         </p>
         </div>`;
 
-      this.setState({content:content});
+      this.setState({ content: content });
 
       // * Adds click event listener to markers
       marker.addListener("click", () => {
