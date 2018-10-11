0cd68da4f24d4e830acb9c86cd1a4fd0942d113a
Jonathan Sexton <joncsexton@gmail.com>
2018-10-11 04:33:52 -0500

    Performance-Changed Map over to stateless component syntax

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,16 +1,7 @@
-import React, { Component } from "react";
-import axios from "axios";
-// import Sidebar from './Sidebar';
+import React from "react";
 
-// //*! API Keys
-// const fs_client_api = `${process.env.REACT_APP_FS_CLIENT}`;
-// const fs_secret_api = `${process.env.REACT_APP_FS_SECRET}`;
-// const GM_API_KEY = `${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}`;
-
-class Map extends Component {
-  render() {
-    return <div role="application" id="map" />;
-  }
-}
+const Map = () => {
+  return <div role="application" id="map" />;
+};
 
 export default Map;
