d24932bf6799ed6cb0ce6285773018cb6cfc1844
Jonathan Sexton <joncsexton@gmail.com>
2018-10-15 05:43:41 -0500

    In Progress-working on filtering markers to hide those not matched

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,42 +2,51 @@ import React, { Component } from "react";
 import SidebarList from "./SidebarList";
 
 class Sidebar extends Component {
-constructor() {
-  super();
-  this.state = {
-    query: "",
-  };
-}
-
-  filterVenueListings = () => {
-
+  constructor() {
+    super();
+    this.state = {
+      query: "",
+    };
   }
+
+  filterVenueListings = () => {};
 
   handleInputChange = (event) => {
-    this.setState({ query: event.target.value })
+    this.setState({ query: event.target.value });
     const markers = this.props.venues.map((venue) => {
-      const firstMatched = venue.name.toLowerCase().includes(event.target.value.toLowerCase());
-      const marker = this.props.markers.find(marker => marker.id === venue.id);
+      const firstMatched = venue.venue.name
+        .toLowerCase()
+        .includes(event.target.value.toLowerCase());
+      const marker = this.props.markers.find(
+        (marker) => marker.id === venue.venue.id
+      );
+      // console.log("venue id - " + venue.venue.id)
+      // console.log("marker id - " + marker.id)
       if (firstMatched) {
-        firstMatched.isVisible = true;
+        marker.isVisible = true;
       } else {
-        firstMatched.isVisible = false;
+        marker.isVisible = false;
       }
       return marker;
-    })
-  }
+    });
+    this.props.updateSuperState({ markers });
+  };
 
   render() {
     return (
       <aside id="sidebarContainer">
         <h1 id="sidebarHeader">San Antonio Parks</h1>
         <label htmlFor="filter">Search Venues</label>
-        <input id="filter" type="search" placeholder="amusement park" />
+        <input
+          id="filter"
+          type="search"
+          placeholder="amusement park"
+          onChange={this.handleInputChange}
+        />
         <SidebarList
           {...this.props}
           handleVenueClick={this.props.handleVenueClick}
-          onChange={this.handleInputChange}
-          />
+        />
       </aside>
     );
   }
