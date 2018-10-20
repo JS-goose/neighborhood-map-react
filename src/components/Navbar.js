feb48cf6f039eb95859846ef1c96c430e9a86194
Jonathan Sexton <joncsexton@gmail.com>
2018-10-20 05:34:27 -0500

    Added Navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Navbar extends Component {
+    render() {
+        return(
+            <div>
+                This is the Navbar
+            </div>
+
+        )
+    }
+}
+
+export default Navbar
